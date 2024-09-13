88fe83f8c3ee56159d467e04d780135dfdca8385
Yosef <yosef@indeed.com>
2024-09-14 00:46:00 +0900

    add frontEnd

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 const { PORT = 5001 } = process.env;
 
 const app = require("./app");
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'frontEnd')));
+
+// Serve index.html for the root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontEnd', 'index.html'));
+});
+
+
 const knex = require("./db/connection");
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
