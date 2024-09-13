4530d55f933b87215e31613e243f432088518e06
Yosef <yosef@indeed.com>
2024-09-13 16:39:04 +0900

    add core to app

--- src/app.js
+++ src/app.js
@@ -8,10 +8,12 @@ const errorHandler = require("./errors/errorHandler");
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
+const cors = require('cors');
 
 // TODO: Add your code here
 ///movies
 app.use(express.json());
+app.use(cors());
 
 app.use("/movies", moviesRouter);
 app.use("/theaters", theatersRouter);
